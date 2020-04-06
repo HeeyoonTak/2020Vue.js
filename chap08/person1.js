866e552d5c453e3fb3bc310e9bd64512fc5bfbe1
Heeyoon <wewe1003@naver.com>
2020-04-06 21:53:19 +0900

    4주차 강의2

--- chap08/person1.js
+++ chap08/person1.js
@@ -1,5 +1,10 @@
-let person = { name: "홍길동", age: 16 };
-
-console.log(person);
-console.log(person.name);
-console.log(person.age);
+class Person {
+    constructor(s, i) {
+      this.name = s;
+      this.age = i;
+    } 
+  }
+  
+  let person = new Person("홍길동", 16);
+  console.log(person);
+  
